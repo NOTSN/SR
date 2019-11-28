dc65e442bb9df0d8b769e44c029408eb3f8576b8
duan <481239368@qq.com>
2019-11-28 21:27:25 +0800

    第一次提交

---
+++ Inc/main.h
@@ -0,0 +1,112 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define OLED_SCL_Pin GPIO_PIN_0
+#define OLED_SCL_GPIO_Port GPIOC
+#define OLED_SDA_Pin GPIO_PIN_1
+#define OLED_SDA_GPIO_Port GPIOC
+#define OLED_RES_Pin GPIO_PIN_2
+#define OLED_RES_GPIO_Port GPIOC
+#define OLED_DC_Pin GPIO_PIN_3
+#define OLED_DC_GPIO_Port GPIOC
+#define LED1_Pin GPIO_PIN_6
+#define LED1_GPIO_Port GPIOA
+#define LED2_Pin GPIO_PIN_7
+#define LED2_GPIO_Port GPIOA
+#define OLED_CS_Pin GPIO_PIN_4
+#define OLED_CS_GPIO_Port GPIOC
+#define OLED_BLK_Pin GPIO_PIN_5
+#define OLED_BLK_GPIO_Port GPIOC
+/* USER CODE BEGIN Private defines */
+
+
+//-----------------OLED¶Ë¿Ú¶¨Òå---------------- 
+
+#define OLED_SCLK_Clr() HAL_GPIO_WritePin(OLED_SCL_GPIO_Port,OLED_SCL_Pin,GPIO_PIN_RESET)//SCL
+#define OLED_SCLK_Set() HAL_GPIO_WritePin(OLED_SCL_GPIO_Port,OLED_SCL_Pin,GPIO_PIN_SET)
+
+#define OLED_SDIN_Clr() HAL_GPIO_WritePin( OLED_SDA_GPIO_Port,OLED_SDA_Pin,GPIO_PIN_RESET)//DIN
+#define OLED_SDIN_Set() HAL_GPIO_WritePin( OLED_SDA_GPIO_Port,OLED_SDA_Pin,GPIO_PIN_SET)
+
+#define OLED_RES_Clr() HAL_GPIO_WritePin( OLED_RES_GPIO_Port,OLED_RES_Pin,GPIO_PIN_RESET)//RES
+#define OLED_RES_Set() HAL_GPIO_WritePin( OLED_RES_GPIO_Port,OLED_RES_Pin,GPIO_PIN_SET)
+
+#define OLED_DC_Clr() HAL_GPIO_WritePin( OLED_DC_GPIO_Port,OLED_DC_Pin,GPIO_PIN_RESET)//DC
+#define OLED_DC_Set() HAL_GPIO_WritePin( OLED_DC_GPIO_Port,OLED_DC_Pin,GPIO_PIN_SET)
+ 		     
+#define OLED_CS_Clr()  HAL_GPIO_WritePin( OLED_CS_GPIO_Port,OLED_CS_Pin,GPIO_PIN_RESET)//CS
+#define OLED_CS_Set()  HAL_GPIO_WritePin( OLED_CS_GPIO_Port,OLED_CS_Pin,GPIO_PIN_SET)
+
+#define OLED_BLK_Clr()  HAL_GPIO_WritePin( OLED_BLK_GPIO_Port,OLED_BLK_Pin,GPIO_PIN_RESET)//BLK
+#define OLED_BLK_Set()  HAL_GPIO_WritePin( OLED_BLK_GPIO_Port,OLED_BLK_Pin,GPIO_PIN_SET)
+
+void HAL_Delay_us(uint16_t nus);
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
